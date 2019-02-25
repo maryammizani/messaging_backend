8d2d3d8948cd64d07a4a8dbb5f15a861115beedc
mmizani <maryam.mizani@gmail.com>
2019-02-24 19:55:47 -0500

    Add error handling + serve static images

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const feedRoutes = require('./routes/feed');
 const app = express();
@@ -10,6 +11,11 @@ const MONGODB_URI = key.MONGODB_URI;
 
 //app.use(bodyParser.urlencoded()); /// x-www-form-urlencoded <form>
 app.use(bodyParser.json());  //application/json
+
+// Serve the images staticlly
+// All the req starting with /images, will go through this middleware
+// __dirname: path to the dir were this file is located
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
     // set all the domains that should be able to access our server
@@ -21,6 +27,13 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({  message: message });
+});
 
 mongoose.connect(MONGODB_URI + '?retryWrites=true', { useNewUrlParser: true } )
 .then(result => {
--- controllers/feed.js
+++ controllers/feed.js
@@ -20,10 +20,13 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);  // The validation errors that were gathered in routes are saved here
     if(!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+        // return res.status(422).json({
+        //     message: 'Validation failed, entered data is incorrect.',
+        //     errors: errors.array()
+        // });
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -46,6 +49,9 @@ exports.createPost = (req, res, next) => {
         });
     })
     .catch(err => {
-        console.log(err);
+        if(!err.stausCode) {
+            err.stattusCode = 500;
+        }
+        next(err);
     });
 }
