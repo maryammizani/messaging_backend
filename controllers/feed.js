15ed1e35c7aafd2384fdabfccd9a335f2ec17632
mmizani <maryam.mizani@gmail.com>
2019-02-26 05:49:22 -0500

    Update a post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const {validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -19,7 +21,9 @@ exports.getPosts = (req, res, next) => {
 }
 
 exports.createPost = (req, res, next) => {
-    const errors = validationResult(req);  // The validation errors that were gathered in routes are saved here
+
+    // Check validation errors that were gathered in routes 
+    const errors = validationResult(req);  
     if(!errors.isEmpty()) {
         const error = new Error('Validation failed, entered data is incorrect.');
         error.statusCode = 422;
@@ -84,4 +88,65 @@ exports.getPost = (req, res, next) => {
         }
         next(err);
     });
+}
+
+exports.updatePost = (req, res, next) => {
+
+    // Check validation errors that were gathered in routes 
+    const errors = validationResult(req); 
+    if(!errors.isEmpty()) {
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    const postId = req.params.postId;
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;  // If no new file is added, the previoud path is used
+    if(req.file) {   // If user enters a new file to replace the old one
+        imageUrl = req.file.path;
+        if(process.platform === "win32")
+        {
+            imageUrl = imageUrl.replace("\\" ,"/");
+        }
+    }
+    if(!imageUrl) {
+        const error = new Error('No file picked.');
+        error.statusCode
+    }
+
+    // Update the post in DB
+    Post.findById(postId)
+    .then(post => {
+        if(!post) {
+            const error = new Error('Could not find post.');
+            error.statusCode = 404;
+            throw error;
+        }
+        if(!imageUrl !== post.imageUrl) {
+            clearImage(post.imageUrl);
+        }
+        post.title = title;
+        post.imageUrl = imageUrl;
+        post.content = content;
+        return post.save();
+    })
+    .then(result => {
+        res.status(200).json({
+            message: 'Post updated!',
+            post: result
+        });
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });  
+}
+
+const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));  //Asynchronously removes the file
 }
