75240ecf1567314ce6768cb4652777632ed9b2a3
mmizani <maryam.mizani@gmail.com>
2019-02-27 06:27:09 -0500

    Add user signup validation and controller

---
+++ controllers/auth.js
@@ -0,0 +1,18 @@
+const {validationResult} = require('express-validator/check');
+const User = require('../models/user');
+
+exports.signup = (req, res, next) => {
+
+    // Check the validation errors that were gathered in routes
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const error = new Error('Validation failed.');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    
+}
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         required: true
