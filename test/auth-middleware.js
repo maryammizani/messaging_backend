266c7e6f1749c03a1e4885f620f75fedcd538d91
mmizani <maryam.mizani@gmail.com>
2019-03-26 05:18:10 -0400

    Use sinon to add a stub for jwt.verify

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,7 @@
 const expect = require('chai').expect;
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
+
 const authMiddleware = require('../middleware/is-auth');
 
 describe('Auth middleware', function(){
@@ -20,5 +23,30 @@ describe('Auth middleware', function(){
         }
         expect(authMiddleware.bind(this,
             req, {}, () => {})).to.throw();
+    })
+
+    it('should throw an error if the token can not be verified', function() {
+        const req = {
+            get: function() {
+                return 'Bearer xyz';
+            }
+        }
+        expect(authMiddleware.bind(this,
+            req, {}, () => {})).to.throw();
+    })
+
+    it('should yields userId after decoding the token', function() {
+        const req = {
+            get: function() {
+                return 'Bearer xyz';
+            }
+        };
+        sinon.stub(jwt, 'verify');
+        jwt.verify.returns({userId: 'abc'});
+        authMiddleware(req, {}, () => {});
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId', 'abc');
+        expect(jwt.verify.called).to.be.true;
+        jwt.verify.restore();
     })
 })
